df629f81691cddcd9b794617ebb31cde0df956ba
Pete Gadomski <pete.gadomski@gmail.com>
2020-10-19 08:38:21 -0600

    Find external leeward

---
+++ FindLeeward.cmake
@@ -0,0 +1,27 @@
+find_path(Leeward_INCLUDE_DIR
+    NAMES leeward.h
+    PATHS ${Leeward_DIR}
+)
+find_library(Leeward_LIBRARY
+    NAMES libleeward.a
+    PATHS ${Leeward_DIR}
+)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Leeward
+    FOUND_VAR Leeward_FOUND
+    REQUIRED_VARS
+        Leeward_INCLUDE_DIR
+        Leeward_LIBRARY
+)
+
+if(Leeward_FOUND AND NOT TARGET Leeward::Leeward)
+    add_library(Leeward::Leeward UNKNOWN IMPORTED)
+    set_target_properties(Leeward::Leeward PROPERTIES
+        IMPORTED_LOCATION "${Leeward_LIBRARY}" 
+        INTERFACE_INCLUDE_DIRECTORIES "${Leeward_INCLUDE_DIR}"
+    )
+endif()
+
+mark_as_advanced(Leeward_INCLUDE_DIR)
+mark_as_advanced(Leeward_LIBRARY)
