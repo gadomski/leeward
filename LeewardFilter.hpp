3f474b5a2bbbdb93319e41eab5f304460b9bc720
Pete Gadomski <pete.gadomski@gmail.com>
2020-10-19 08:18:15 -0600

    Initial commit

---
+++ LeewardFilter.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <pdal/Filter.hpp>
+#include <pdal/pdal_internal.hpp>
+
+namespace pdal
+{
+    class PDAL_DLL LeewardFilter : public Filter
+    {
+    public:
+        LeewardFilter() : Filter() {}
+
+        std::string getName() const;
+
+    private:
+        virtual void addDimensions(PointLayoutPtr layout);
+        virtual void addArgs(ProgramArgs &args);
+        virtual void filter(PointView &view);
+
+        std::string m_sbet_path;
+        std::string m_config_path;
+        int m_quantization;
+        Dimension::Id m_sigmaX;
+        Dimension::Id m_sigmaY;
+        Dimension::Id m_sigmaHorizontal;
+        Dimension::Id m_sigmaVertical;
+        Dimension::Id m_sigmaMagnitude;
+    };
+} // namespace pdal
