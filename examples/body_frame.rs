ee0a2e5f0c07b9eee68dee227ded33f3a723703f
Pete Gadomski <pete.gadomski@gmail.com>
2020-12-16 13:25:50 -0700

    Working on body frame

--- examples/body_frame.rs
+++ examples/body_frame.rs
@@ -13,27 +13,39 @@ fn main() {
         )
         .arg(Arg::with_name("points").takes_value(true).required(true))
         .arg(Arg::with_name("config").takes_value(true).required(true))
+        .arg(
+            Arg::with_name("decimate")
+                .takes_value(true)
+                .short("d")
+                .long("decimate"),
+        )
         .get_matches();
     let trajectory = Trajectory::from_path(matches.value_of("trajectory").unwrap()).unwrap();
     let config = Config::from_path(matches.value_of("config").unwrap()).unwrap();
+    let step = matches.value_of("decimate").unwrap_or("1").parse().unwrap();
     println!("Time,X,Y,Z,BodyFrameX,BodyFrameY,BodyFrameZ");
     for point in Reader::from_path(matches.value_of("points").unwrap())
         .unwrap()
         .points()
         .map(|r| r.unwrap())
+        .step_by(step)
     {
-        let measurement = Measurement::new(&trajectory, point, config).unwrap();
-        let las = measurement.las();
-        let body_frame = measurement.body_frame();
-        println!(
-            "{},{},{},{},{},{},{}",
-            measurement.time(),
-            las.x,
-            las.y,
-            las.z,
-            body_frame.x,
-            body_frame.y,
-            body_frame.z
-        );
+        match Measurement::new(&trajectory, point, config) {
+            Ok(measurement) => {
+                let las = measurement.las();
+                let body_frame = measurement.body_frame();
+                println!(
+                    "{},{},{},{},{},{},{}",
+                    measurement.time(),
+                    las.x,
+                    las.y,
+                    las.z,
+                    body_frame.x,
+                    body_frame.y,
+                    body_frame.z
+                );
+            }
+            Err(err) => eprintln!("{}", err),
+        }
     }
 }
