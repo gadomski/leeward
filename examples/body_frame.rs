ad5ccf134b4bc5b15389ff71e300785d9b737297
Pete Gadomski <pete.gadomski@gmail.com>
2020-12-29 08:53:34 -0700

    Just provide scan angle

--- examples/body_frame.rs
+++ examples/body_frame.rs
@@ -23,7 +23,7 @@ fn main() {
     let trajectory = Trajectory::from_path(matches.value_of("trajectory").unwrap()).unwrap();
     let config = Config::from_path(matches.value_of("config").unwrap()).unwrap();
     let step = matches.value_of("decimate").unwrap_or("1").parse().unwrap();
-    println!("Time,X,Y,Z,BodyFrameX,BodyFrameY,BodyFrameZ,BodyFrameConfigX,BodyFrameConfigY,BodyFrameConfigZ");
+    println!("Time,X,Y,Z,BodyFrameX,BodyFrameY,BodyFrameZ,ScanAngle");
     for point in Reader::from_path(matches.value_of("points").unwrap())
         .unwrap()
         .points()
@@ -33,9 +33,8 @@ fn main() {
         match Measurement::new(&trajectory, point, config) {
             Ok(measurement) => {
                 let body_frame = measurement.body_frame();
-                let body_frame_config = measurement.body_frame_from_config();
                 println!(
-                    "{},{},{},{},{},{},{},{},{},{}",
+                    "{},{},{},{},{},{},{},{}",
                     measurement.time(),
                     measurement.x(),
                     measurement.y(),
@@ -43,9 +42,7 @@ fn main() {
                     body_frame.x,
                     body_frame.y,
                     body_frame.z,
-                    body_frame_config.x,
-                    body_frame_config.y,
-                    body_frame_config.z,
+                    measurement.scan_angle(),
                 );
             }
             Err(err) => eprintln!("{}", err),
