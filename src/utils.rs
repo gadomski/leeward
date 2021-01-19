649b90375e0adc9f8146024f60d2d357f9dda06b
Pete Gadomski <pete.gadomski@gmail.com>
2021-01-19 14:11:35 -0700

    Working on best fit plane, but going to change to "model" language

--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,11 @@
 //! Utility functions.
 
-use crate::{Lasish, Measurement, Point};
+use crate::{Lasish, Matrix, Measurement, Point};
+use nalgebra::{Dynamic, MatrixMN, U3};
 
 /// Fits a bunch of measurements to a plane in the platform's body frame.
 ///
-/// Returns each measurement projected onto the plane, with the z value being the distance from the plane.
+// Returns each measurement projected onto the plane, with the z value being the distance from the plane.
 ///
 /// # Examples
 ///
@@ -14,6 +15,29 @@ use crate::{Lasish, Measurement, Point};
 /// let points = utils::fit_to_plane_in_body_frame(measurements);
 /// assert_eq!(measurements.len(), points.len());
 /// ```
-pub fn fit_to_plane_in_body_frame<L: Lasish>(_measurements: &[Measurement<L>]) -> Vec<Point> {
+pub fn fit_to_plane_in_body_frame<L: Lasish>(measurements: &[Measurement<L>]) -> Vec<Point> {
+    let mut points = MatrixMN::<f64, Dynamic, U3>::zeros(measurements.len());
+    for (i, measurement) in measurements.iter().enumerate() {
+        let body_frame = measurement.body_frame();
+        points[(i, 0)] = body_frame.x;
+        points[(i, 1)] = body_frame.y;
+        points[(i, 2)] = body_frame.z;
+    }
+    let centroid = points.row_mean();
+    for (i, mean) in centroid.iter().enumerate() {
+        points.set_column(i, &points.column(i).add_scalar(-mean));
+    }
+    let svd = points.transpose().svd(true, false);
+    let _u = svd.u.unwrap();
     unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn best_fitting_plane() {
+        let measurements =
+            crate::measurements("data/sbet.out", "data/points.las", "data/config.toml").unwrap();
+        let _points = super::fit_to_plane_in_body_frame(&measurements);
+    }
 }
